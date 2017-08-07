4d7edeb4561b4cbfb1074fe139e283e471822570
Rahat Mahbub <rahatm1@uvic.ca>
2017-08-06 20:45:32 -0700

    Merged all optimizations

--- cordic_optimized/cordic_V_fixed_point.c
+++ cordic_optimized/cordic_V_fixed_point.c
@@ -2,7 +2,7 @@
 #include <stdint.h>
 #include "cordic_fixed_point.h"
 
-void cordic_R_fixed_point(int *x, int *y, int *z)
+void cordic_V_fixed_point(int *x, int *y, int *z)
 {
     register int i asm("r4");
     register int x_temp_1 asm("r5");
@@ -14,7 +14,7 @@ void cordic_R_fixed_point(int *x, int *y, int *z)
 
     x_temp_1 = *x;
     y_temp_1 = *y;
-    z_temp_1 = *z;
+    z_temp_1 = 0;
 
     register uint32_t z_tmp_loader asm("r1");
     register uint16_t first_angle asm("r2");
@@ -31,7 +31,7 @@ void cordic_R_fixed_point(int *x, int *y, int *z)
 
         z_preload = z_table[ (i+2) >> 1];
 
-        if( z_temp_1<0)
+        if( y_temp_1 > 0)
         {
             x_temp_2 = x_temp_1 + (y_temp_1 >> i);
             y_temp_2 = y_temp_1 - (x_temp_1 >> i);
@@ -47,7 +47,7 @@ void cordic_R_fixed_point(int *x, int *y, int *z)
         y_temp_1 = y_temp_2;
         i++;
 
-        if( z_temp_1<0)
+        if(y_temp_1 > 0)
         {
             x_temp_2 = x_temp_1 + (y_temp_1 >> i);
             y_temp_2 = y_temp_1 - (x_temp_1 >> i);
